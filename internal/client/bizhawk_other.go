31d8c56b9bbdd6a5df603e097e756e91f43be6f9
Michael Harvey <michael4d45@gmail.com>
2025-10-31 17:14:00 -0600

    clean up code

--- internal/client/bizhawk_other.go
+++
@@ -1,9 +0,0 @@
-//go:build !windows
-
-package client
-
-import "log"
-
-func (c *BizHawkController) checkAndInstallVCRedist() {
-	log.Printf("Skipping VC++ redistributable check on non-Windows platform")
-}
